e468d75a27f59c949c24acfc40a087602c1728c1
EdenGenetTasama <edentasama19@gmail.com>
2021-12-01 11:01:40 +0200

    .

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-import myName from "./module.js";
+// import {myName , printingMyName} from "./module.js";
+// printingMyName("Eden");
+// console.log(myName);
 
-console.log(myName);
+// import numberOne from "./module.js";
+// console.log(numberOne);
+
+// import textOne from "./module.js";
+// console.log(textOne);
+
+
+// import objOne from "./module.js";
+// console.log(objOne.age);
+
+import {numberTwo , objTwo ,boolOne,textTwo } from "./module.js";
+import returnSumArray from "./module.js";
+// console.log(numberTwo , objTwo.name ,bool,textTwo);
+
+let arrayToSum = [10,4,50,40,32,78];
+console.log(returnSumArray(arrayToSum));
+
--- module.js
+++ module.js
@@ -1,7 +1,54 @@
-const myName = "Eden" ; 
+// const myName = "Eden" ;
 
-function printingMyName(){
-    console.log(myName+" Tasama");
+// function printingMyName(name){
+//     console.log(name +" Tasama");
+// }
+
+// // printingMyName()
+// // export default myName;
+
+// export {myName ,printingMyName } ;
+
+///הגדר משתנה מסוג מספר ויצא אותו בעזרת export default.
+
+// const numberOne = 18;
+
+// export default numberOne;
+
+//הגדר משתנה מסוג טקסט ויצא אותו בעזרת export default.
+
+// const textOne= "Eden Genet Tasama";
+// export default textOne;
+
+//הגדר משתנה מסוג אובייקט ויצא אותו בעזרת export default.
+
+// const objOne= {
+//     name:"Eden Genet",
+//     lastN: "Tasama",
+//     age: 25,
+
+// }
+
+// export default objOne;
+
+//הגדר משתנה מסוג מספר, אובייקט, בוליאני וטקסט ויצא את כולם.
+
+const numberTwo = 5;
+const objTwo = {
+  name: "Eden Genet",
+  lastN: "Tasama",
+  age: 25,
+};
+const boolOne = true;
+const textTwo = "Love";
+
+function returnSumArray(array) {
+  let sumOfArray = 0;
+  for (let i = 0; i < array.length; i++) {
+    sumOfArray += array[i];
+  }
+  return sumOfArray;
 }
-// printingMyName()
-export default myName;
+
+// export default returnSumArray;
+export default {numberTwo , objTwo ,boolOne,textTwo,returnSumArray} ;
