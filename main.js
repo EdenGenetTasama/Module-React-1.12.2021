fad1b01dd5129111ea2a45c116c0e34258c1210c
EdenGenetTasama <edentasama19@gmail.com>
2021-12-01 09:31:13 +0200

    .

---
+++ main.js
@@ -0,0 +1,3 @@
+import myName from "./module.js";
+
+console.log(myName);
---
+++ module.js
@@ -0,0 +1,7 @@
+const myName = "Eden" ; 
+
+function printingMyName(){
+    console.log(myName+" Tasama");
+}
+// printingMyName()
+export default myName;
